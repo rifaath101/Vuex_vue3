9cf5f1186c642bf8c47b91bd3d1b747a2f8ba844
Rifaath <rifaath.mohamedrifki@tamara.co>
2023-03-21 17:27:59 +0400

    Using mapper helpers

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <base-container title="Vuex">
     <counter></counter>
-    <button @click="addOne">Add 10</button>
+    <button @click="increase({ value: 10 })">Add 10</button>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import Counter from './components/Counter.vue';
+import { mapActions } from 'vuex';
+
 export default {
   components: {
     BaseContainer,
     Counter,
   },
   methods: {
-    addOne() {
-      // this.$store.commit('increase', { value: 10 });
-      this.$store.dispatch('increment');
-    },
+    // addOne() {
+    // this.$store.commit('increase', { value: 10 });
+    // this.$store.dispatch('increment');
+    ...mapActions(['increment', 'increase']),
   },
 };
 </script>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,3 +1,13 @@
 <template>
-  <h3>{{ this.$store.getters.normalizedCounter }}</h3>
+  <h3>{{ finalCounter }}</h3>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['finalCounter']),
+  },
+};
+</script>
