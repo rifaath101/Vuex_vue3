46ac7dbda557ff3c56986c61257606a3c82824b4
Rifaath <rifaath.mohamedrifki@tamara.co>
2023-03-27 16:01:51 +0400

    Organizing your store with modules

--- src/main.js
+++ src/main.js
@@ -3,25 +3,22 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const store = createStore({
-  state() { // Just data
+const counterModule = {
+  state() {
     return {
       counter: 0,
-      isLoggedIn: false
     }
   },
-  mutations: { // Use mutations to change the state of your data, state is given automatically as an argument. Having one place to change the state is better than having multiple places because if the logic changes, you can change the mutation in just one place. Don't edit the state in vuex directly in other components, it should always be done in mutations
+  mutations: {
+    // Use mutations to change the state of your data, state is given automatically as an argument. Having one place to change the state is better than having multiple places because if the logic changes, you can change the mutation in just one place. Don't edit the state in vuex directly in other components, it should always be done in mutations
     increment(state) {
       state.counter = state.counter + 1;
     } ,
     increase(state, payload) { // Your mutations can take a second argument named payload in which it can be any type of data. You could need this perhaps in this scenario to increase with different values in different components.
       state.counter = state.counter + payload.value
     },
-    setAuth(state, payload) {
-      state.isLoggedIn = payload.isAuth // You add a key to the payload called isAuth which will have the value of isLoggedIn
-    }
   },
-  actions: { // You can use actions to run your mutations asynchronously 
+  actions: {
     increment(context) {
       setTimeout( function() {
         context.commit('increment') // This is how you call your mutations in actions. your mutation will be a string
@@ -30,14 +27,8 @@ const store = createStore({
     increase(context, payload) { // You add a payload as a second argument since it's required for the increase mutation to work as increase requires a payload
       context.commit('increase', payload)
     },
-    login(context) {
-      context.commit('setAuth', {isAuth: true}) // you can modify the value of isAuth and once isAuth is modified, it will reflect on isLoggedIn as well
-    },
-    logout(context) {
-      context.commit('setAuth', {isAuth: false} )
-    }
   },
-  getters: { // Getters can modify or combine data to create a new state property. These getters are read-only and do not mutate the state. To mutate the state, use mutations
+  getters: {
     finalCounter(state) {
       return state.counter * 3
     },
@@ -51,6 +42,32 @@ const store = createStore({
       }
       return finalCounter
     },
+  }, 
+}
+
+const store = createStore({
+  modules: {
+    numbers: counterModule 
+  },
+  state() { // Just data
+    return {
+      isLoggedIn: false
+    }
+  },
+  mutations: { 
+    setAuth(state, payload) {
+      state.isLoggedIn = payload.isAuth // You add a key to the payload called isAuth which will have the value of isLoggedIn
+    }
+  },
+  actions: { // You can use actions to run your mutations asynchronously 
+    login(context) {
+      context.commit('setAuth', {isAuth: true}) // you can modify the value of isAuth and once isAuth is modified, it will reflect on isLoggedIn as well
+    },
+    logout(context) {
+      context.commit('setAuth', {isAuth: false} )
+    }
+  },
+  getters: { // Getters can modify or combine data to create a new state property. These getters are read-only and do not mutate the state. To mutate the state, use mutations
     userIsAuthenticated(state) {
       return state.isLoggedIn
     }
